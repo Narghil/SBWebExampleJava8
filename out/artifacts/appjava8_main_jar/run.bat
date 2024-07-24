9291f39b69b9fa39382ce5b86c10f7f8733d959e
Pernyei Gábor <45963704+Narghil@users.noreply.github.com>
2024-07-24 21:29:02 +0200

    Initial commit

---
+++ out/artifacts/appjava8_main_jar/run.bat
@@ -0,0 +1,2 @@
+del *.txt
+"C:\Program Files\Java\jdk1.8.0_281\bin\java.exe" -jar appjava8.main.jar 1> log.txt 2>err.txt
---
+++ src/main/java/com/appjava8/AppJava8.java
@@ -0,0 +1,12 @@
+package com.appjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJava8 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.appjava8.AppJava8.class, args);
+	}
+}
